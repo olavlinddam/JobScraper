c97925ca1d6f361380290c15c606cc467cb1bfed
Olav Lind-Dam <olavlinddam@proton.me>
2024-11-26 22:04:39 +0100

    navigation for ef mapping

--- JobScraper.Domain/Entities/Website.cs
+++ JobScraper.Domain/Entities/Website.cs
@@ -3,10 +3,13 @@ namespace JobScraper.Domain.Entities;
 public class Website
 {
     public string Id { get; set; }
-    public string Uri { get; set; }
+    public string Url { get; set; }
     public string ShortName { get; set; }
     public DateTime LastScraped { get; set; }
+
+    
+    public int ScrapingPolicyId { get; set; }
     public ScrapingPolicy ScrapingPolicy { get; set; }
-    public ICollection<JobListing> Jobs { get; set; } = new List<JobListing>();
+    public ICollection<JobListing> JobsListings { get; set; } = new List<JobListing>();
     public ICollection<ScrapingError> ScrapingErrors { get; set; } = new List<ScrapingError>();
 }
