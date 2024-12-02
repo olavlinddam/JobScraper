e6c2ffc04a6578177ecff9cb39379e98d4e9ae0d
Olav <olavlinddam@proton.me>
2024-12-02 20:54:13 +0100

    ..

--- JobScraper.Infrastructure/Scrapers/JobnetScraper.cs
+++ JobScraper.Infrastructure/Scrapers/JobnetScraper.cs
@@ -1,13 +1,17 @@
 using ErrorOr;
 using JobScraper.Application.Features.Scraping.Common;
 using JobScraper.Domain.Entities;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
 
 namespace JobScraper.Infrastructure.Scrapers;
 
 public class JobnetScraper : IWebScraper
 {
+    private readonly IWebDriver _driver = new ChromeDriver();
+    private readonly string _baseUrl = ""
     public Task<ErrorOr<string>> ScrapePageAsync(string url, ScrapingPolicy policy, CancellationToken cancellationToken)
     {
-        throw new NotImplementedException();
+        
     }
 }
