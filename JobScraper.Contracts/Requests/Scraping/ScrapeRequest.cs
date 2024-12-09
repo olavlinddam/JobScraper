04df2fdd67cb916b1a26ed722bfc15102bbab476
Olav <olavlinddam@proton.me>
2024-12-09 21:53:29 +0100

    simplifying constructor parameters

--- JobScraper.Contracts/Requests/Scraping/ScrapeRequest.cs
+++ JobScraper.Contracts/Requests/Scraping/ScrapeRequest.cs
@@ -1,3 +1,5 @@
 namespace JobScraper.Contracts.Requests.Scraping;
 
-public record ScrapeRequest(string WebsiteBaseUrl, string WebsiteShortName, List<string> SearchTerms);
+public record ScrapeRequest(
+    string WebsiteBaseUrl, 
+    List<string> SearchTerms);
