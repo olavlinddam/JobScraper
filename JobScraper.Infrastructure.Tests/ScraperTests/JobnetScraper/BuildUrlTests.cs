7f8e8f63ea130bc5356210b9b3b00821da134ef5
Olav <olavlinddam@proton.me>
2024-12-09 21:49:06 +0100

    Had to change the test methods because of a major refactor in JobnetScraper.cs

--- JobScraper.Infrastructure.Tests/ScraperTests/JobnetScraper/BuildUrlTests.cs
+++ JobScraper.Infrastructure.Tests/ScraperTests/JobnetScraper/BuildUrlTests.cs
@@ -10,42 +10,44 @@ public class BuildUrlTests
     public void BuildUrl_WithSearchTermOnly_ShouldReturnSingleParameterUrl()
     {
         // Arrange
-        var website = new Website()
+        var url = "https://job.jobnet.dk/CV/FindWork?Offset=0&SortValue=BestMatch";
+        var searchTerms = new List<string>() { "Systemudvikling, programmering og design" };
+        var scrapeRequest = new ScrapeRequest(url, searchTerms);
+
+        // Act
+        var result = Scrapers.JobnetScraper.BuildUrls(scrapeRequest);
+
+        // Assert
+        var expected = new List<string>()
         {
-            Url = "https://job.jobnet.dk/CV/FindWork?Offset=0&SortValue=BestMatch"
+            "https://job.jobnet.dk/CV/FindWork?Offset=0&SortValue=BestMatch&SearchString=%2522Systemudvikling,%2520" +
+            "programmering%2520og%2520design%2522"
         };
 
-        var scrapeRequest = new ScrapeRequest()
-        {
-            SearchTerm = "Systemudvikling, programmering og design"
-        };
-        
-        // Act
-        var result = Scrapers.JobnetScraper.BuildUrls(website, scrapeRequest);
-        
-        // Assert
-        Assert.AreEqual("https://job.jobnet.dk/CV/FindWork?Offset=0&SortValue=BestMatch&SearchString=%2522Systemudvikling,%2520programmering%2520og%2520design%2522", result);
-    } 
-    
-    [TestMethod]
-    public void BuildUrl_WithZipAndDistance_ShouldReturnSingleParameterUrl()
-    {
-        // Arrange
-        var website = new Website()
-        {
-            Url = "https://job.jobnet.dk/CV/FindWork?Offset=0&SortValue=BestMatch"
-        };
+        Assert.AreEqual(expected[0], result[0]);
+    }
 
-        var scrapeRequest = new ScrapeRequest()
-        {
-            Location = "5000",
-            DistanceFromLocation = "50"
-        };
-        
-        // Act
-        var result = Scrapers.JobnetScraper.BuildUrls(website, scrapeRequest);
-        
-        // Assert
-        Assert.AreEqual("https://job.jobnet.dk/CV/FindWork?Offset=0&SortValue=BestMatch&LocationZip=5000&SearchInGeoDistance=50", result);
-    } 
+    // [TestMethod]
+    // public void BuildUrl_WithZipAndDistance_ShouldReturnSingleParameterUrl()
+    // {
+    //     // Arrange
+    //     var website = new Website()
+    //     {
+    //         Url = "https://job.jobnet.dk/CV/FindWork?Offset=0&SortValue=BestMatch"
+    //     };
+    //
+    //     var scrapeRequest = new ScrapeRequest()
+    //     {
+    //         Location = "5000",
+    //         DistanceFromLocation = "50"
+    //     };
+    //
+    //     // Act
+    //     var result = Scrapers.JobnetScraper.BuildUrls(website, scrapeRequest);
+    //
+    //     // Assert
+    //     Assert.AreEqual(
+    //         "https://job.jobnet.dk/CV/FindWork?Offset=0&SortValue=BestMatch&LocationZip=5000&SearchInGeoDistance=50",
+    //         result);
+    // }
 }
