a6a6997f50ccc9cb7c7c5acb470ff0e18b3c322c
Olav <olavlinddam@proton.me>
2024-12-12 14:03:11 +0100

    Extracting zip as well as city name

--- JobScraper.Application/Features/Scraping/Common/LocationParser.cs
+++ JobScraper.Application/Features/Scraping/Common/LocationParser.cs
@@ -11,4 +11,14 @@ public static class LocationParser
 
         return parts.Length == 1 ? parts[0] : parts[1];
     }
+
+    public static int ExtractZipCode(string location)
+    {
+        if (string.IsNullOrEmpty(location))
+            return 0;
+        
+        var parts = location.Split(' ');
+        
+        return parts.Length == 1 ? int.Parse(parts[0]) : int.Parse(parts[1]);
+    }
 }
