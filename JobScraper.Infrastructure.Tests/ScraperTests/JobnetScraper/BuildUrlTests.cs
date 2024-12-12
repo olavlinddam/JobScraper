77ea6cf9c0c4f4dd555c580fa568f0eb5bd1ddbe
Olav <olavlinddam@proton.me>
2024-12-12 21:14:33 +0100

    updated to match new params of BuildUrl method in jobnetscraper

--- JobScraper.Infrastructure.Tests/ScraperTests/JobnetScraper/BuildUrlTests.cs
+++ JobScraper.Infrastructure.Tests/ScraperTests/JobnetScraper/BuildUrlTests.cs
@@ -11,43 +11,16 @@ public class BuildUrlTests
     {
         // Arrange
         var url = "https://job.jobnet.dk/CV/FindWork?Offset=0&SortValue=BestMatch";
-        var searchTerms = new List<string>() { "Systemudvikling, programmering og design" };
-        var scrapeRequest = new ScrapeRequest(url, searchTerms);
+        var searchTerm = "Systemudvikling, programmering og design";
 
         // Act
-        var result = Scrapers.JobnetScraper.BuildUrls(scrapeRequest);
+        var result = Scrapers.JobnetScraper.BuildUrl(searchTerm, url);
 
         // Assert
-        var expected = new List<string>()
-        {
+        var expected =
             "https://job.jobnet.dk/CV/FindWork?Offset=0&SortValue=BestMatch&SearchString=%2522Systemudvikling,%2520" +
-            "programmering%2520og%2520design%2522"
-        };
+            "programmering%2520og%2520design%2522";
 
         Assert.AreEqual(expected[0], result[0]);
     }
-
-    // [TestMethod]
-    // public void BuildUrl_WithZipAndDistance_ShouldReturnSingleParameterUrl()
-    // {
-    //     // Arrange
-    //     var website = new Website()
-    //     {
-    //         Url = "https://job.jobnet.dk/CV/FindWork?Offset=0&SortValue=BestMatch"
-    //     };
-    //
-    //     var scrapeRequest = new ScrapeRequest()
-    //     {
-    //         Location = "5000",
-    //         DistanceFromLocation = "50"
-    //     };
-    //
-    //     // Act
-    //     var result = Scrapers.JobnetScraper.BuildUrls(website, scrapeRequest);
-    //
-    //     // Assert
-    //     Assert.AreEqual(
-    //         "https://job.jobnet.dk/CV/FindWork?Offset=0&SortValue=BestMatch&LocationZip=5000&SearchInGeoDistance=50",
-    //         result);
-    // }
 }
