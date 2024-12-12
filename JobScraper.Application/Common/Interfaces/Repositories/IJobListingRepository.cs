31bbaf808892adb28f4a9d2625b6ddcc6d36aebd
Olav <olavlinddam@proton.me>
2024-12-12 14:02:50 +0100

    fixing commit error from earlier

--- JobScraper.Application/Common/Interfaces/Repositories/IJobListingRepository.cs
+++ JobScraper.Application/Common/Interfaces/Repositories/IJobListingRepository.cs
@@ -4,6 +4,8 @@ namespace JobScraper.Application.Common.Interfaces.Repositories;
 
 public interface IJobListingRepository
 {
+
     Task AddAsync(JobListing listing, CancellationToken cancellationToken);
+
     IQueryable<JobListing> GetRecentListings(CancellationToken cancellationToken);
 }
